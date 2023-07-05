6bbc333afd0846d9d5d00b6fcdc3059da0bfbd43
junhyukM <minjh1031@gmail.com>
2023-07-05 10:05:38 +0900

    M대 N 데이터 추가, 조회

--- movies/models.py
+++ movies/models.py
@@ -4,10 +4,13 @@ from django.db import models
 
 class Actor(models.Model):
     name = models.CharField(max_length=10)
-
+    # movie_set = 눈에는 안보이지만 자동생성, 1대 N 에서도 언급된 내용
+    # a1.movie_set.all() 이런식으로 사용 a1.movie_set.add()
 
 class Movie(models.Model):
     title = models.CharField(max_length=20)
+    actors = models.ManyToManyField(Actor)
+
 
 
 
